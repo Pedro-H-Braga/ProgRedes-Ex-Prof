b174e961b0145df233cb93762428a54da363aefc
charles-freitas <ccmfreitas@gmail.com>
2023-04-12 19:11:00 -0300

    Atualização de Repositório

---
+++ 2023-04-12 - Revisão/tse_eleicoes.py
@@ -0,0 +1,42 @@
+'''
+    O TSE divulga na sua página oficial um webservice que fornece os dados 
+    das apurações das eleições realizadas no Brasil.
+
+    O fragmento de código a seguir monta um dicionário (dados_retorno) com o 
+    resultado das eleições do ano de 2022 no 1º turno para Presidente.
+
+    Com base na documentação da API contida na URL 
+    https://www.tse.jus.br/eleicoes/eleicoes-2022/interessados-na-divulgacao-de-resultados-2022
+
+
+    Pede-se que seja desenvolvido um programa que solicite ao usuário o 
+    tipo de eleição (estadual, nacional) e o cargo eletivo e o programa 
+    deverá montar um dicionário {k:v} no seguinte formato:
+    {
+        num_candidato: { 'nome ': nome_candidato, 'partido': nome_partido, 
+                         'votos': quantidade_votos, 
+                         'percentual': percentual_votos},
+        num_candidato: { 'nome ': nome_candidato, 'partido': nome_partido, 
+                         'votos': quantidade_votos, 
+                         'percentual': percentual_votos},
+        ...
+    }
+    
+    O dicionário deverá ser ordenado de forma decrescente pela quantidade de
+    votos que o candidato obteve.
+
+    Em seguida, deverá ser gerado um arquivo (resultados.txt) onde na 
+    primeira linha deverá constar a seguinte string:
+        numero;nome,partido;quantidade_votos;percentual_votos
+
+    Da segunda linha em diante deverão constar os dados correspondentes de
+    cada candidato
+'''
+
+import requests
+
+url = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
+
+dados_retorno = requests.get(url).json()
+
+print(dados_retorno)
